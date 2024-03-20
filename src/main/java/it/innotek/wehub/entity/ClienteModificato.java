daba6163660f057bf7c9f19bb049c9ee631ca120
marco.sciuto <marco.sciuto@innoma.it>
2024-03-20 15:43:45 +0100

    Aziende dettagli

--- src/main/java/it/innotek/wehub/entity/ClienteModificato.java
+++ src/main/java/it/innotek/wehub/entity/ClienteModificato.java
@@ -49,6 +49,11 @@ public class ClienteModificato implements Serializable {
 
     private Owner owner;
 
+    private String sedeOperativa;
+
+    private String settoreMercato;
+
+
     @Override
     public boolean equals(Object o) {
 
