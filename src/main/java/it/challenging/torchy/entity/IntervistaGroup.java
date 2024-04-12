51a68bbc910412575ca7e8ba0456d13c647a0bc0
msciuto <m.sciuto@inno-tek.it>
2024-04-12 13:50:28 +0200

    Group

--- src/main/java/it/challenging/torchy/entity/IntervistaGroup.java
+++ src/main/java/it/challenging/torchy/entity/IntervistaGroup.java
@@ -8,11 +8,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -23,24 +21,5 @@ public class IntervistaGroup implements Serializable {
     private Long record;
 
     private List<IntervistaModificato> interviste;
-
-    @Override
-    public boolean equals(Object o) {
-
-        if (this == o) {
-            return true;
-        }
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
-            return false;
-        }
-        IntervistaGroup candidato = (IntervistaGroup)o;
-        return record != null && Objects.equals(record, candidato.record);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return getClass().hashCode();
-    }
 
 }
