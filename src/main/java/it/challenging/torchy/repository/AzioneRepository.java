608cceac1aef016827dd74a5c482cea16b6a122d
msciuto <m.sciuto@inno-tek.it>
2024-04-16 14:05:12 +0200

    BugFixing su azioni

--- src/main/java/it/challenging/torchy/repository/AzioneRepository.java
+++ src/main/java/it/challenging/torchy/repository/AzioneRepository.java
@@ -15,12 +15,13 @@ import java.util.List;
 public interface AzioneRepository extends JpaRepository<AzioneKeyPeople,Integer> {
 
   @Query(value= """
-       SELECT a.*
-       FROM key_people k, key_people_azioni ka, azioni a
-       where k.id = ka.id_key_people
-       and ka.id_azione = a.id
-       and k.id = ?1
-       order by k.nome asc
+       SELECT a.*, ta.id_tipologia
+       FROM key_people k
+        left join key_people_azioni ka on k.id = ka.id_key_people
+        left join azioni a on ka.id_azione = a.id
+        left join tipologia_azione ta on ta.id_azione = a.id
+       where k.id = ?1
+       order by a.data_modifica desc
       """,nativeQuery=true)
   List<AzioneKeyPeople> findByKeyPeople(Integer id);
 
