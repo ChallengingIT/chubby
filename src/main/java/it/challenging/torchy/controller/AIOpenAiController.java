9723f75e95f1faaad984b576bf9f9d14d43e6b7d
msciuto <m.sciuto@inno-tek.it>
2024-04-19 13:35:59 +0200

    First implementation

--- src/main/java/it/challenging/torchy/controller/AIOpenAiController.java
+++ src/main/java/it/challenging/torchy/controller/AIOpenAiController.java
@@ -4,6 +4,10 @@
 
 package it.challenging.torchy.controller;
 
+import it.challenging.torchy.entity.Candidato;
+import it.challenging.torchy.entity.Tipologia;
+import it.challenging.torchy.repository.CandidatoRepository;
+import it.challenging.torchy.repository.TipologiaRepository;
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -14,6 +18,7 @@ import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.ai.openai.OpenAiImageClient;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
@@ -25,11 +30,24 @@ import java.util.Map;
 @RequestMapping("/ai/open")
 public class AIOpenAiController {
 
+    @Autowired
+    private TipologiaRepository tipologiaRepository;
+
+    @Autowired
+    private CandidatoRepository candidatoRepository;
+
     private final EmbeddingClient embeddingClient;
 
     private final OpenAiChatClient chatClient;
 
     private final OpenAiImageClient openaiImageClient;
+
+    private static final String SYSTEM_MESSAGE = """
+            Sei un utilissimo server API che risponse in formato JSON.
+            Non sai altro. Rispondi solo con il JSON.
+            
+            L'utente ti farà una domanda sui nostri dati e tu risponderai in JSON.
+            """;
 
     public AIOpenAiController(EmbeddingClient embeddingClient, OpenAiChatClient chatClient, OpenAiImageClient openaiImageClient) {
 
@@ -55,16 +73,28 @@ public class AIOpenAiController {
         return chatClient.stream(prompt);
     }
 
+    @GetMapping("/try")
+    public Map generateStreamWithDB(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        List<Tipologia> tipologieJson = tipologiaRepository.findAll();
+        List<Candidato> candidatoList = candidatoRepository.findAll();
+
+        message = message + " cerca il messaggio precedente in questa lista " + candidatoList;
+
+        return Map.of("generation", chatClient.call(message));
+    }
+
     @GetMapping("/image")
     public ImageResponse generateImage(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        return openaiImageClient.call(
-            new ImagePrompt(message,
-                OpenAiImageOptions.builder()
-                                  .withQuality("hd")
-                                  .withN(4)
-                                  .withHeight(1024)
-                                  .withWidth(1024).build())
+        ImageResponse response = openaiImageClient.call(
+                new ImagePrompt(message,
+                        OpenAiImageOptions.builder()
+                                .withQuality("hd")
+                                .withN(4)
+                                .withHeight(1024)
+                                .withWidth(1024).build())
 
         );
+
+        return response;
     }
 }
