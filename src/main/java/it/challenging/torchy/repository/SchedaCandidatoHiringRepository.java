7508d774d127488ed76a1be282edb96ad65caffa
msciuto <m.sciuto@inno-tek.it>
2024-07-29 10:22:41 +0200

    Cancellazione scheda candidato

--- src/main/java/it/challenging/torchy/repository/SchedaCandidatoHiringRepository.java
+++ src/main/java/it/challenging/torchy/repository/SchedaCandidatoHiringRepository.java
@@ -5,11 +5,13 @@
 package it.challenging.torchy.repository;
 
 import it.challenging.torchy.entity.SchedaCandidatoHiring;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SchedaCandidatoHiringRepository extends JpaRepository<SchedaCandidatoHiring, Integer> {
 
+    @Transactional
     void deleteByIdSchedaCandidato(Integer idScheda);
 }
