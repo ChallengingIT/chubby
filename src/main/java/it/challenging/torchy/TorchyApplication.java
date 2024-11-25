f2b5eb9ba4e77f3031628f98f4880e97bd8d7ed1
msciuto <m.sciuto@inno-tek.it>
2024-11-25 17:29:28 +0100

    testing

--- src/main/java/it/challenging/torchy/TorchyApplication.java
+++ src/main/java/it/challenging/torchy/TorchyApplication.java
@@ -5,7 +5,9 @@
 package it.challenging.torchy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication(scanBasePackages={"it.challenging.torchy"})
 public class TorchyApplication {
 
 	public static void main(String[] args) {
